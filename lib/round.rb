da25888c13b094b899c1b4414522163203e0752f
margdaly <margaret.daly@gmail.com>
2023-11-18 13:56:25 -0700

    feat: Add take and record turn to round

--- lib/round.rb
+++ lib/round.rb
@@ -2,8 +2,7 @@
 
 # Round will be the object that processes responses and records guesses
 class Round
-  attr_reader :deck
-  attr_accessor :turns
+  attr_accessor :deck, :turns
 
   # initialize will take a deck as an argument
   # and start with an empty array of turns
@@ -13,6 +12,25 @@ class Round
   end
 
   def current_card
-    deck.cards.first
+    @deck.cards.first
+  end
+
+  def take_turn(guess)
+    turn = Turn.new(guess, current_card)
+    @turns << turn
+    @deck.cards.shift
+    turn
+  end
+
+  def number_correct
+    @turns.count(&:correct?)
+  end
+
+  def number_correct_by_category(category)
+    @turns.count { |turn| turn.correct? && turn.card.category == category }
+  end
+
+  def last_turn_feedback
+    @turns.last.feedback
   end
 end
