782d75a048437b768476ebe200ad9ed8cc3bbf9a
margdaly <margaret.daly@gmail.com>
2023-11-18 12:56:50 -0700

    test: Add tests for round exists with a deck

---
+++ spec/round_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Round do
+  let (:card_1) { Card.new('What does the acronym "HTML" stand for in web development?', 'HyperText Markup Language', :STEM) }
+  let (:card_2) { Card.new('In computer programming, what does the term "API" stand for?', 'Application Programming Interface', :STEM) }
+  let (:card_3) { Card.new('On which continent are polar bears exclusively found?', 'Antarctica', :Geography) }
+  let (:deck) { Deck.new([card_1, card_2, card_3]) }
+  let (:round) { Round.new(deck) }
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(round).to be_instance_of(Round)
+    end
+
+    it 'is initialized with a deck' do
+      expect(round.deck).to eq(deck)
+    end
+  end
+end
