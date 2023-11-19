7e03e005372c05b2f7c0e41765b2a9fb6cf2b204
margdaly <margaret.daly@gmail.com>
2023-11-18 17:45:44 -0700

    test: Update card_3 with correct answer for geo

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Round do
   let (:card_1) { Card.new('What does the acronym "HTML" stand for in web development?', 'HyperText Markup Language', :STEM) }
   let (:card_2) { Card.new('In computer programming, what does the term "API" stand for?', 'Application Programming Interface', :STEM) }
-  let (:card_3) { Card.new('On which continent are polar bears exclusively found?', 'Antarctica', :Geography) }
+  let (:card_3) { Card.new('On which continent are kangaroos exclusively found?', 'Australia', :Geography) }
   let (:deck) { Deck.new([card_1, card_2, card_3]) }
   let (:round) { Round.new(deck) }
 
@@ -51,6 +51,32 @@ describe Round do
 
     it 'returns the next card in the deck as the current card' do
       expect(round.current_card).to eq(card_2)
+    end
+  end
+
+  describe 'taking multiple turns' do
+    before :each do
+      round.take_turn('HyperText Markup Language')
+      round.take_turn('Application Programming Interface')
+      round.take_turn('Africa')
+    end
+
+    it 'records the number of correct guesses' do
+      expect(round.number_correct).to eq(2)
+    end
+
+    it 'records the number of correct guesses by category' do
+      expect(round.number_correct_by_category(:STEM)).to eq(2)
+      expect(round.number_correct_by_category(:Geography)).to eq(0)
+    end
+
+    it 'returns the percent of correct guesses' do
+      expect(round.percent_correct).to eq(66.7)
+    end
+
+    it 'returns the percent of correct guesses by category' do
+      expect(round.percent_correct_by_category(:STEM)).to eq(100.0)
+      expect(round.percent_correct_by_category(:Geography)).to eq(0.0)
     end
   end
 end
